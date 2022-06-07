3138f00c635e71dacb80bab8696aa80c3f54b8e2
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-07 18:42:31 +0200

    snack-2/completato

--- snack-2/index.php
+++ snack-2/index.php
@@ -7,6 +7,33 @@
   Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 */
 
+$autorizzazione_accesso = true;
+
+
+// verifico l'esistenza dei parametri necessari in $_GET
+if(empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])){
+  $autorizzazione_accesso = false;
+
+}elseif(strlen($_GET['name']) < 3 ){  
+  // verifico la lunghezza di name
+  $autorizzazione_accesso = false;
+
+}elseif(!strpos($_GET['mail'], '@') || !strpos($_GET['mail'], '.')){
+  // verifico se sono presenti @ e .
+  $autorizzazione_accesso = false;
+
+}elseif(!is_numeric($_GET['age'])){
+  // verifico che age sia un numero
+  $autorizzazione_accesso = false;
+}
+
+
+if($autorizzazione_accesso ){
+  $login = 'Accesso riuscito';
+}else{
+  $login = 'Accesso negato';
+}
+
 ?>
 
 
@@ -19,6 +46,13 @@
   <title>Snack 2</title>
 </head>
 <body>
+  <header>
+    <h1>Snack 2</h1>
+  </header>
+
+  <main>
+    <h3> <?php echo $login ?> </h3>
+  </main>
   
 </body>
 </html>
