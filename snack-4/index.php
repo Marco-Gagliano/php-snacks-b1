98bca7eeaaa82b05b6c20269a182d35f4f4f2a3c
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-07 18:13:43 +0200

    snack-4/completato

--- snack-4/index.php
+++ snack-4/index.php
@@ -5,6 +5,26 @@
   Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
 */
 
+$arrayNumeri = [];
+$nItem = 15;
+
+// creo un ciclo while con "count" che conta tutti gli elementi nell'array "arrayNumeri",
+// fino a che non minore/uguale di "nItem"
+while (count($arrayNumeri) <= $nItem) {
+
+  // creo una variabile in cui genero dei numeri da 1 a 100
+  $num = rand(1, 100);
+
+  //creo una condizione in cui verifico se il numero non è presente nell'array; 
+  if (!in_array($num, $arrayNumeri)) {
+
+    // in caso positivo lo pusho
+    $arrayNumeri[] = $num;
+  }
+}
+
+var_dump($arrayNumeri)
+
 ?>
 
 
@@ -17,6 +37,7 @@
   <title>Snack 4</title>
 </head>
 <body>
-  
+
+
 </body>
 </html>
